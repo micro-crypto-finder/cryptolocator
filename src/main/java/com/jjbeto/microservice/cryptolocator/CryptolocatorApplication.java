4f3805cec382a4251bc7539321beee254ff8a373
jjbeto <jjbeto@gmail.com>
2022-05-22 14:30:37 +0200

    first commit (spring initializr)

---
+++ src/main/java/com/jjbeto/microservice/cryptolocator/CryptolocatorApplication.java
@@ -0,0 +1,13 @@
+package com.jjbeto.microservice.cryptolocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptolocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptolocatorApplication.class, args);
+	}
+
+}
